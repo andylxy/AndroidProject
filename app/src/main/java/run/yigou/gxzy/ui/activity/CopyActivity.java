33cd31b80ebea1c75159d813a0184c595f4c086f
zhs <xiaoyang_02@qq.com>
2023-07-05 17:11:32 +0800

    改包名,程序图标及程序名称

---
+++ app/src/main/java/run/yigou/gxzy/ui/activity/CopyActivity.java
@@ -0,0 +1,28 @@
+package run.yigou.gxzy.ui.activity;
+
+import run.yigou.gxzy.R;
+import run.yigou.gxzy.app.AppActivity;
+
+/**
+ *    author : Android 轮子哥
+ *    github : https://github.com/getActivity/AndroidProject
+ *    time   : 2018/10/18
+ *    desc   : 可进行拷贝的副本
+ */
+public final class CopyActivity extends AppActivity {
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.copy_activity;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
---
+++ app/src/main/java/run/yigou/gxzy/ui/fragment/CopyFragment.java
@@ -0,0 +1,33 @@
+package run.yigou.gxzy.ui.fragment;
+
+import run.yigou.gxzy.R;
+import run.yigou.gxzy.app.AppFragment;
+import run.yigou.gxzy.ui.activity.CopyActivity;
+
+/**
+ *    author : Android 轮子哥
+ *    github : https://github.com/getActivity/AndroidProject
+ *    time   : 2018/10/18
+ *    desc   : 可进行拷贝的副本
+ */
+public final class CopyFragment extends AppFragment<CopyActivity> {
+
+    public static CopyFragment newInstance() {
+        return new CopyFragment();
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.copy_fragment;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
